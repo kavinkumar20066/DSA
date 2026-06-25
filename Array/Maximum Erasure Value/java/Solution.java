dba48c5fdc41f1c7c48b8a4ac30f603e9831c751
KavinKumar S <skavinkumar16k@gmail.com>
2026-06-25 16:11:23 +0530

    feat: Add Maximum Erasure Value

--- Array/Maximum Erasure Value/java/Solution.java
+++ Array/Maximum Erasure Value/java/Solution.java
@@ -1,16 +1,22 @@
 class Solution {
-    public int maximumUniqueSubarray(int[] n)
+    public int maximumUniqueSubarray(int[] n) 
     {
-        TreeSet<Integer> set=new TreeSet<>();
+        int right=0;
+        int max=0;
         int sum=0;
+        HashSet<Integer> set=new HashSet<>();
         for(int i=0;i<n.length;i++)
         {
+            while(!set.isEmpty() && set.contains(n[i]))
+            {
+                set.remove(n[right]);
+                sum=sum-n[right];
+                right++;
+            }
             set.add(n[i]);
-        }   
-        for(Integer k:set)
-        {
-            sum+=k;
+            sum=sum+n[i];
+            max=Math.max(sum,max);
         }
-        return sum;
+        return max;
     }
 }
