3108829fd65a9f0e45f2ca3e54073fe161f34fb8
KavinKumar S <skavinkumar16k@gmail.com>
2026-06-27 16:20:38 +0530

    feat: Add Fibonacci Number

--- Math/Fibonacci Number/java/Solution.java
+++ Math/Fibonacci Number/java/Solution.java
@@ -1,13 +1,8 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-    int[] F=new int[n+1];
-    F[0]=0;
-    F[1]=1;
-    for(int i=2;i<F.length;i++){
-        F[i]=F[i-2]+F[i-1];
-    }
-    return F[n];
+        if(n<2){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
     }
 }
