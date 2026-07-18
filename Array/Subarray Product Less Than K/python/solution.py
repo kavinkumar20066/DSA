468e5a77fbf7f38d28d278db64e586fffa91a2d3
KavinKumar S <skavinkumar16k@gmail.com>
2026-07-18 21:16:48 +0530

    feat: Add Subarray Product Less Than K

---
+++ Array/Subarray Product Less Than K/python/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        product=1
+        sum=0
+        left=0
+        for i in range(len(nums)):
+            product=product*nums[i]
+            while product>k:
+                product=product/nums[left]
+                left=left+1
+            sum=sum+((i-left)+1)
+        return sum
+        
