ccb92d2bb241b01582ef1425a25ce1691e24c680
KavinKumar S <skavinkumar16k@gmail.com>
2026-07-18 21:21:49 +0530

    feat: Add Subarray Product Less Than K

--- Array/Subarray Product Less Than K/python/solution.py
+++ Array/Subarray Product Less Than K/python/solution.py
@@ -5,7 +5,7 @@ class Solution:
         left=0
         for i in range(len(nums)):
             product=product*nums[i]
-            while product>=k:
+            while product>=k and left<len(nums):
                 product=product/nums[left]
                 left=left+1
             sum=sum+((i-left)+1)
