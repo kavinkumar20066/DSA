8756194065431f4e02d5e7f2fc724e2923c63f39
KavinKumar S <skavinkumar16k@gmail.com>
2026-07-19 22:54:38 +0530

    feat: Add Sqrt(x)

--- Math/Sqrt(x)/java/Solution.java
+++ Math/Sqrt(x)/java/Solution.java
@@ -1,23 +1,5 @@
 class Solution {
-    public int mySqrt(int x) 
-    {
-        if(x==0) return 0;
-        int l=0;
-        int h=x;
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
-            long s=(long)mid*mid;
-            if((s)==x) return mid;
-            else if((s)>x)
-            {
-                h=mid-1;
-            }
-            else
-            {
-                l=mid+1;
-            }
-        }   
-        return h;
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
     }
 }
