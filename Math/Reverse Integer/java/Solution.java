37aee069d19b81512cd86c35eb2f297c86457512
KavinKumar S <skavinkumar16k@gmail.com>
2026-07-20 22:23:19 +0530

    feat: Add Reverse Integer

---
+++ Math/Reverse Integer/java/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        
+        int rem=0;
+        long sum=0;
+        while(x!=0){
+            rem=x%10;
+            sum=sum*10+rem;
+            x/=10;
+             
+        }
+         if(sum>Integer.MAX_VALUE||sum<Integer.MIN_VALUE){
+             return 0;
+        }
+      
+        return (int)sum;
+    }
+}
