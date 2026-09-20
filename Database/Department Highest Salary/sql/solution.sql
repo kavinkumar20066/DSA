45ccaff5d5d2cc43ef753d1d83b24159d1b43f66
KavinKumar S <skavinkumar16k@gmail.com>
2026-09-20 16:13:57 +0530

    feat: Add Department Highest Salary

--- Database/Department Highest Salary/sql/solution.sql
+++ Database/Department Highest Salary/sql/solution.sql
@@ -1,4 +1,12 @@
-# Write your MySQL query statement below
-select department,employee,salary from 
-(select e.name department ,d.name employee,rank() over(partition by d.id order by salary desc) rnk,salary from employee e left join department d on e.departmentid=d.id )t 
-where rnk=1
+# Write your MySQL query statement below
+#select department,employee,salary from 
+
+select e.name department,
+    d.name employee,
+    rank() over(partition by d.id order by salary desc) rnk,
+    salary 
+
+
+from employee e left join department d 
+on e.departmentid=d.id 
+#where rnk=1
